6e1489025d8bec7bf16451b3541114df6adb03c4
Dorothy Rifai <drifai28@gmail.com>
2022-02-02 09:34:26 -0500

    Restrict Data Initialization to local profile

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,11 +3,13 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 6/12/21.
  */
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -19,24 +21,18 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if(bookRepository.count() == 0) {
-            Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
 
-            System.out.println("Id: " + bookDDD.getId() );
+        bookRepository.deleteAll();
 
-            Book savedDDD = bookRepository.save(bookDDD);
+        Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+        Book savedDDD = bookRepository.save(bookDDD);
 
-            System.out.println("Id: " + savedDDD.getId() );
+        Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
+        Book savedSIA = bookRepository.save(bookSIA);
 
-            Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
-            Book savedSIA = bookRepository.save(bookSIA);
-
-            bookRepository.findAll().forEach(book -> {
-                System.out.println("Book Id: " + book.getId());
-                System.out.println("Book Title: " + book.getTitle());
-            });
-        }
-
-
+        bookRepository.findAll().forEach(book -> {
+            System.out.println("Book Id: " + book.getId());
+            System.out.println("Book Title: " + book.getTitle());
+        });
     }
 }
