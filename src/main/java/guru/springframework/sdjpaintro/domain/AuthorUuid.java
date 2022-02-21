7d85c5afc9b575a90837606d1f62590e3b11ecd2
Dorothy Rifai <drifai28@gmail.com>
2022-02-20 20:58:57 -0500

    UUID primary key

---
+++ src/main/java/guru/springframework/sdjpaintro/domain/AuthorUuid.java
@@ -0,0 +1,71 @@
+package guru.springframework.sdjpaintro.domain;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Created by jt on 6/12/21.
+ */
+@Entity
+public class AuthorUuid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false )
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+
+    public AuthorUuid() {
+
+    }
+
+    public AuthorUuid(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AuthorUuid author = (AuthorUuid) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/guru/springframework/sdjpaintro/repositories/AuthorUuidRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.sdjpaintro.repositories;
+
+import guru.springframework.sdjpaintro.domain.AuthorUuid;
+import guru.springframework.sdjpaintro.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by jt on 6/12/21.
+ */
+public interface AuthorUuidRepository extends JpaRepository<AuthorUuid, Long> {
+}
